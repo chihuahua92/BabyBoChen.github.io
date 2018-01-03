d3b61c578d1398c0987efe83aa470060e01f50ed
BabyBoChen <35010886+BabyBoChen@users.noreply.github.com>
2018-01-04 06:34:37 +0800

    Update screenOff.js

--- screenOff.js
+++ screenOff.js
@@ -22,3 +22,27 @@ $(function(){
 		$("#homeKeyArea").slideDown("slow");
 	});
 });
+
+
+$(function(){
+	$( "#upBarBackgroundColor" ).mouseover(function(){
+		$( "#upBarBackgroundColor").css("opacity",".4");
+	})
+	.mouseout(function() {
+		$( "#upBarBackgroundColor").css("opacity","0");
+	});
+});
+
+$(function(){
+	$("#upBarBackgroundColor").click(function(){
+		$("#upScreenMenu").slideDown("slow");
+	});
+});
+
+$(function(){
+	$("#closeUpScreenMenu").click(function(){
+		$("#upScreenMenu").slideUp("slow");
+	});
+});
+
+
