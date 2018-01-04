ec83367bd54e0ffbe330635b81a86fa6e76db77b
BabyBoChen <35010886+BabyBoChen@users.noreply.github.com>
2018-01-05 05:20:34 +0800

    Update homeScreen.js

--- homeScreen.js
+++ homeScreen.js
@@ -1,11 +1,11 @@
 function changeTime(){
-    var H;
-    var M;
     var currentTime = new Date();
+	var H;
+    var M;
     var currentHours = currentTime.getHours();
     if (currentHours < 10) { H = ("0" + String(currentHours))}
     else { H = String(currentHours)}
-    var currentMinutes = String(currentTime.getMinutes());
+    var currentMinutes = currentTime.getMinutes();
     if (currentMinutes < 10) { M = ("0" + String(currentMinutes))}
     else { M = String(currentMinutes)}
     var timeDisplay = H.concat(":",M)
@@ -14,6 +14,14 @@ function changeTime(){
 }
 document.addEventListener("DOMContentLoaded", function(){ changeTime(); }, false);
 
+
+document.getElementById("app1").addEventListener("click", function(){
+    chrome.app.window.create("lockScreen.html", {
+        frame: "none",
+		innerBounds: { width: 360, height: 640 },
+        resizable: false,
+},close);
+})
 
 
 document.getElementById("app2").addEventListener("click", function(){
