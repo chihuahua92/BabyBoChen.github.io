1e50fcac13d7edc3f2e64dac7d5bb006cd6bff62
BabyBoChen <35010886+BabyBoChen@users.noreply.github.com>
2020-03-08 01:55:50 +0800

    Delete spike.js

--- scripts/spike.js
+++
@@ -1,8 +0,0 @@
-/*左邊玩家剩餘的牌數,每0.1秒一次*/
-function cardsNum(){
-	document.getElementById("cardNum").innerHTML = "You have " + myCards.length + " cards";
-	setTimeout(function(){
-		cardsNum();
-	},100)
-}
-cardsNum();
