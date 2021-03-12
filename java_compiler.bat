06c4afba07151710b8c324e7180d514c643101ac
BabyBoChen <35010886+BabyBoChen@users.noreply.github.com>
2021-03-12 18:16:44 +0800

    Add files via upload

--- java_compiler.bat
+++ java_compiler.bat
@@ -1,13 +1,12 @@
-set "JAVA_HOME=D:\Program Files\Java\jdk-15.0.2\"
+echo off
+set "JAVA_HOME=C:\Program Files\Java\jdk-11.0.6\"
 set java="%JAVA_HOME%bin\java.exe"
 set javac="%JAVA_HOME%bin\javac.exe"
 set "src=%cd%\src"
 set "bin=%cd%\bin"
-set "cp2=C:\Users\User\Desktop\App\java_project\Debug_Java"
-set "mainclass=%src%\app.java"
-set "javafx=C:\Users\User\Desktop\App\java_project\Debug_Java\lib\javafx-sdk-11.0.2\lib"
+set "cp2=%cd%\lib\javafx-sdk-11.0.2\lib"
+set "mainclass=%src%\App.java"
+set "javafx=%cd%\lib\javafx-sdk-11.0.2\lib"
 set "add-modules=javafx.controls,javafx.fxml,javafx.swing"
 %javac% -d %bin% %mainclass% -cp "%bin%;%cp2%" --module-path "%javafx%" --add-modules %add-modules%
 %java% -cp "%bin%;%cp2%" --module-path "%javafx%" --add-modules %add-modules% App
-
-
