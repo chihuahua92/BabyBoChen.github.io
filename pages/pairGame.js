c69fb7a84090c9f79d6deb1151a240918cc97160
BabyBoChen <tc10131@hotmail.com>
2021-10-10 18:56:39 +0800

    Update pairGame.js

--- pages/pairGame.js
+++ pages/pairGame.js
@@ -3,13 +3,14 @@ export const pairGame = `
 
 <h1 style="text-align:center;">JS釣魚小遊戲</h1>
 <div style="text-align:center;">
-    <img style="width:50%;max-width:360px;border:1px solid black" src="https://i.imgur.com/UxosB8q.png"/>
-    <img style="width:50%;max-width:360px;border:1px solid black" src="https://i.imgur.com/BFnCspj.png"/>
+    <img style="width:50%;max-width:360px;border:1px solid black;margin:5px;" src="https://i.imgur.com/UxosB8q.png"/>
+    
+    <img style="width:50%;max-width:360px;border:1px solid black;margin:5px;" src="https://i.imgur.com/BFnCspj.png"/>
 </div>
 <div class="row">
     <div class="col-sm-6">
         <h2>說明：</h2>
-        <p>以Vanilla JS為遊戲引擎、結合canvas繪圖api實作出的2D網頁小遊戲。遊戲勝利條件為翻出所有相同點數且相同顏色的牌（紅或黑）。</p>
+        <p>以Vanilla JS為遊戲引擎、採用canvas繪圖api實作出的2D網頁小遊戲。遊戲勝利條件為翻出所有相同點數且相同顏色的牌（紅或黑）。</p>
     </div>
     <div class="col-sm-6">
         <h2>連結：</h2>
