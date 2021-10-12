0e3a4a55523d5bd3f23064c7b26b9c0a782f2d26
BabyBoChen <tc10131@hotmail.com>
2021-10-12 20:30:58 +0800

    add mikasa

---
+++ mikasa/mathTool.js
@@ -0,0 +1,13 @@
+
+
+export function radians_to_degrees(radians)
+{
+  var pi = Math.PI;
+  return radians * (180/pi);
+}
+
+export function degrees_to_radians(degrees)
+{
+  var pi = Math.PI;
+  return degrees * (pi/180);
+}
