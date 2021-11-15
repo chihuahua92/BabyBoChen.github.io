3bb31b2c028347ceb6e9b557a148ae3ae705cb89
BabyBoChen <tc10131@hotmail.com>
2021-11-15 21:50:20 +0800

    Update haterBoy.js

--- pages/haterBoy.js
+++ pages/haterBoy.js
@@ -1,6 +1,21 @@
 /** @type {String} */
 export const haterBoy = `
-
-    <a href="https://babybofood.000webhostapp.com/#/" target="_blank">haterBoy</a>
-
+    <h1 style="width:100%;text-align:center;">靠北伯夷</h1>
+    <div style="text-align:center;">
+        <img style="width:100%;max-width:720px;border:1px solid black" src="https://i.imgur.com/xLrQa6d.png"/>
+    </div>
+    <div class="row">
+    <div class="col-sm-6">
+        <h2>簡介：</h2>
+        <p>採用Vue.js做為前端框架、以PHP撰寫後端功能的一頁式（spa）網站作品。該作品實作了jwt登入驗證、SMTP站內信（Sendinblue）、所見即所得HTML編輯器（TinyMCE）等功能。</p>
+        <p>「靠北伯夷」網站host在免費的000webhost上，並利用000webhost所提供的Mysql資料庫來儲存會員資料和留言板的留言紀錄。</p>
+    </div>
+    <div class="col-sm-6">
+        <h2>連結：</h2>
+        <ul>
+            <li><a href="https://babybofood.000webhostapp.com/" target="_blank">靠北伯夷（000webhost）</a></li>
+            <li>GitHub存放庫：<a href="" target="_blank"></a>尚未開放</li>
+        </ul>
+    </div>
+</div>
 `;
